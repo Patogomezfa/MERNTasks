7982c22f36e4efda7756081cff0a50cc55886e7a
Patogomezfa <patogomezfa@gmail.com>
2021-07-01 17:16:10 -0300

    update

--- src/components/proyectos/NuevoProyecto.js
+++ src/components/proyectos/NuevoProyecto.js
@@ -5,7 +5,7 @@ const NuevoProyecto = () => {
 
     //Obtener el state del formulario
     const proyectosContext = useContext(proyectoContext);
-    const { formulario, mostrarFormulario } = proyectosContext;
+    const { formulario, mostrarFormulario, agregarProyecto } = proyectosContext;
 
 
     //State para Proyecto
@@ -28,10 +28,16 @@ const NuevoProyecto = () => {
         e.preventDefault();
 
         //Validar el proyecto
-
+        if(nombre === '') {
+            return;
+        }
         //Agregar al state
+        agregarProyecto(proyecto);
 
         //Reiniciar el form
+        guardarProyecto({
+            nombre:''
+        })
     }
 
     //Mostrar el formulario
--- src/context/proyectos/proyectoState.js
+++ src/context/proyectos/proyectoState.js
@@ -1,19 +1,29 @@
 import React, { useReducer } from 'react';
 
+import {v4 as uuidv4} from 'uuid';
+
 import ProyectoContext from './proyectoContext';
 import proyectoReducer from './proyectoReducer';
 
-import { FORMULARIO_PROYECTO } from '../../types';
+import { 
+    FORMULARIO_PROYECTO,
+    OBTENER_PROYECTOS,
+    AGREGAR_PROYECTO
+    } from '../../types';
+
 
 const ProyectoState = props => {
 
+    
+    const proyectos = [
+        { id: 1, nombre: 'Tienda Virtual' },
+        { id: 2, nombre: 'Intranet' },
+        { id: 3, nombre: 'Diseño de Sitio Web' },
+        { id: 4, nombre: 'MERN'}
+    ]
+
     const initialState = {
-        proyectos : [
-            { id: 1, nombre: 'Tienda Virtual' },
-            { id: 2, nombre: 'Intranet' },
-            { id: 3, nombre: 'Diseño de Sitio Web' },
-            { id: 4, nombre: 'MERN'}
-        ],
+        proyectos : [],
         formulario : false
     }
 
@@ -27,12 +37,33 @@ const ProyectoState = props => {
         })
     }
 
+    //Obtener los proyectos
+    const obtenerProyectos = () => {
+        dispatch({
+            type: OBTENER_PROYECTOS,
+            payload: proyectos
+        })
+    }
+
+    //Agregar nuevo proyecto
+    const agregarProyecto = proyecto => {
+        proyecto.id = uuidv4();
+        
+        //Insertar el proyecto en el state
+        dispatch({
+            type: AGREGAR_PROYECTO,
+            payload: proyecto
+        })
+    }
+
     return (
         <ProyectoContext.Provider
         value={{
             proyectos: state.proyectos,
             formulario: state.formulario,
-            mostrarFormulario
+            mostrarFormulario,
+            obtenerProyectos,
+            agregarProyecto
         }}>
             {props.children}
         </ProyectoContext.Provider>
